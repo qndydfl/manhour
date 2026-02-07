68987290b2a434e6c7824dd4f50ffff773fd726d
qndydfl <qndydfl@naver.com>
2026-02-07 20:27:43 +0900

    edit_v4

--- config/settings.py
+++ config/settings.py
@@ -23,13 +23,13 @@ allowed_hosts = os.getenv(
     "DJANGO_ALLOWED_HOSTS",
     "qndydfl.pythonanywhere.com,localhost,127.0.0.1",
 )
-ALLOWED_HOSTS = [h.strip() for h in allowed_hosts.split(",") if h.strip()]
+# ALLOWED_HOSTS = [h.strip() for h in allowed_hosts.split(",") if h.strip()]
 
-DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() in ("1", "true", "yes")
+# DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() in ("1", "true", "yes")
 
-# ALLOWDED_HOSTS = ["*"]  # 배포 시에는 위의 주석 처리된 코드를 사용하세요.
+ALLOWDED_HOSTS = ["*"]  # 배포 시에는 위의 주석 처리된 코드를 사용하세요.
 # Application definition
-# DEBUG = True  # 배포 시에는 위의 주석 처리된 코드를 사용하세요.
+DEBUG = True  # 배포 시에는 위의 주석 처리된 코드를 사용하세요.
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -124,11 +124,9 @@ from pathlib import Path
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static"),]
 
 
 # 세션 설정
